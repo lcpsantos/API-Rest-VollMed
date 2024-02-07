88749b5271be36e3767187161a17533dfb714978
Luiz César Pacheco <lcpsantos@gmail.com>
2024-02-06 21:25:20 -0300

    entity Paciente

---
+++ src/main/java/med/voll/api/CorsConfiguration.java
@@ -0,0 +1,15 @@
+//package med.voll.api;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//
+//@Configuration
+//public class CorsConfiguration {
+//
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("http://localhost:3000")
+//                .allowedMethods("GET", "POST", "PUT", "DELETE");
+//    }
+//}
